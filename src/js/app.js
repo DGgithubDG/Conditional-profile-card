03f3fbb56dae63c5868019a687d057b5a6e8bd52
DGgithubDG <111754374+DGgithubDG@users.noreply.github.com>
2022-11-10 14:12:03 +0100

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -28,35 +28,33 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-  if (variables.name == null) variables.name = "Jane";
-  if (variables.lastname == null) variables.lastname = "Doe";
+
+  if (variables.name == null) variables.name = "Ilias";
+  if (variables.lastname == null) variables.lastname = "Dinia";
   if (variables.role == null) variables.role = "Role";
   if (variables.city == null) variables.city = "City";
-  if (variables.Country == null) variables.country = "Country";
+  if (variables.country == null) variables.country = "Country";
+
   if (variables.socialMediaPosition == "position-left") {
     variables.socialMediaPosition = "position-left";
-  } else {
-    variables.socialMediaPosition = "position-right";
-  }
+  } else variables.socialMediaPosition = "position-right";
 
   // reset the website body with the new html output
-
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name} ${variables.lastname}</h1>
-          <h2>${variables.role}</h2>
-          <h3>${variables.city}, ${variables.Country}</h3>
-          <ul ${variables.SocialMediaPosition}">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+                ${cover}
+              <img src="${variables.avatarURL}" class="photo" />
+              <h1>${variables.name} ${variables.lastname}</h1>
+              <h2>${variables.role}</h2>
+              <h3>${variables.city} ${variables.country}</h3>
+              <ul class="${variables.socialMediaPosition}">
+                <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
+                <li><a href="https://linkedin.com/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
+                <li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
+              </ul>
+            </div>
+        `;
 }
-
 /**
  * Don't change any of the lines below, here is where we do the logic for the dropdowns
  */
